90d888f572c18150f884d5bef135a3075e50ec68
efaks <edward.akselrud@gmail.com>
2026-03-22 20:30:11 -0400

    moved home.html to index.html

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,7 @@
 const NAVBAR_HTML = `
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="home.html">Club Portal</a>
+        <a class="navbar-brand" href="index.html">Club Portal</a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
@@ -12,7 +12,7 @@ const NAVBAR_HTML = `
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" href="home.html">Home</a>
+                    <a class="nav-link" href="index.html">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="manageUsers.html">Manage Users</a>
