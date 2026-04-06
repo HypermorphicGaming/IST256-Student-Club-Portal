7d87fa976a9fa75575cab15438500dff208b0924
HypermorphicGaming <xenomorphic120@gmail.com>
2026-04-05 21:58:33 -0400

    Create NavBar.jsx

---
+++ react-club-portal/src/NavBar.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; // if using React Router
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <Link className="navbar-brand" to="/">Student Club Portal</Link>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav ms-auto">  {/* ms-auto pushes links to the right if needed */}
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/register">Register</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/manage-users">Manage Users</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/manage-events">Manage Events</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/checkout">Checkout</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
